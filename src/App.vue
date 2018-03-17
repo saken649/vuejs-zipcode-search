fda56327e5384079fa69858aa1ca20613e0ca1cd
saken649 <>
2018-03-18 01:19:32 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <Header/>
+    <b-row>
+      <b-col md="12" lg="6">
+        <b-form class="margin-top-30">
+          <b-form-input type="text" placeholder="example: 1500001" maxlength="7" class="zip"/>
+          <div class="margin-top-10">
+            <b-button type="button" @click="search()" variant="info">検索</b-button>
+            <b-button type="button" @click="reset()" variant="outline-info">リセット</b-button>
+          </div>
+        </b-form>
+        <b-list-group id="search-result" class="margin-top-30">
+          <b-list-group-item>Prefecture: {{prefecture}}</b-list-group-item>
+          <b-list-group-item>City: {{city}}</b-list-group-item>
+          <b-list-group-item>Address: {{address}}</b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      prefecture: '',
+      city: '',
+      address: ''
+    }
+  },
+  methods: {
+    search () {
+      var url = 'http://zipcloud.ibsnet.co.jp/api/search?zipcode=' + $('.zip').val()
+      var data = this
+      $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'jsonp'
+      }).done(function (json) {
+        console.log(json)
+        if (json.status === 200 && json.results !== null) {
+          var result = json.results[0]
+          data.prefecture = result.address1 + ' (' + result.kana1 + ')'
+          data.city = result.address2 + ' (' + result.kana2 + ')'
+          data.address = result.address3 + ' (' + result.kana3 + ')'
+        } else {
+          if (json.message === null) {
+            alert('存在しない郵便番号のようです。')
+          } else {
+            alert(json.message)
+          }
+        }
+      }).fail(function (xhr) {
+        console.log(xhr)
+      })
+    },
+    reset () {
+      $('.zip').val('')
+      this.prefecture = ''
+      this.city = ''
+      this.address = ''
+    }
+  }
+}
+</script>
+
+<style>
+.margin-top-10 {
+  margin-top: 10px;
+}
+.margin-top-30 {
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+   <b-navbar type="dark" variant="info">
+      <b-navbar-brand href="#">Zipcode Search with Vue.js</b-navbar-brand>
+   </b-navbar>
+</template>
