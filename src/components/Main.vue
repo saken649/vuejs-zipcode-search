1d68003a30f6da83f34cab4d1de47c3c7164a82c
saken649 <>
2018-04-02 23:46:36 +0900

    devブランチ作成用

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,8 +4,8 @@
       <b-form class="margin-top-30">
         <b-form-input type="text" placeholder="example: 1500001" maxlength="7" v-model="zip"/>
         <div class="margin-top-10">
-          <b-button type="button" @click="search()" variant="info">検索</b-button>
-          <b-button type="button" @click="reset()" variant="outline-info">リセット</b-button>
+          <b-button type="button" @click="search()" variant="info">Search</b-button>
+          <b-button type="button" @click="reset()" variant="outline-info">Reset</b-button>
         </div>
       </b-form>
       <b-list-group id="search-result" class="margin-top-30">
